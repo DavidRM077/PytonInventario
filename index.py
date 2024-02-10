429f03b9a09f1c6ca469d6ef3ca8d56061c1c8a1
DavidR <davidrap087@gmail.com>
2024-02-10 15:45:37 -0400

    agregado de la 2da funcion , funcion actualizar

--- index.py
+++ index.py
@@ -8,4 +8,16 @@ def Agregar_Productos(inventario):
     inventario[nombre] = {"descripcion":descripcion,"precio":precio,"cantidad":cantidad}
     print("Producto Agregado")
 
- 
+"""  Funcion para actualizar un producto  """
+
+def Actualizar_Producto(inventario):
+    nombre = input("Ingresa el nombre del producto a actualziar: ")
+    if nombre in inventario:
+        print("Actualice los detalles del producto")
+        descripcion = input(f"Nueva descripción ({inventario[nombre]['descripcion']}): ") or inventario[nombre]['descripcion']
+        precio = float(input(f"Nuevo precio  ({inventario[nombre]["desripcion"]}): ") or inventario[nombre["precio"]]) 
+        cantidad = input(f"Nueva Cantidad ({inventario[nombre]["descripcion"]}):"   or inventario[nombre]["cantidad"])
+        inventario[nombre] = {"descripcion":descripcion,"precio":precio,"cantidad":cantidad}
+        print("el producto fue actualizado en el inventario")
+    else:
+        print("el producto no existe en el inventario")
